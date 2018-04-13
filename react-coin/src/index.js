d6f8fa42ba3108d7385d7a65437b628bcdb69387
phixuannhat <phixuannhat@gmail.com>
2018-04-13 10:23:12 -0500

    - use route to display components - add a catch-all not-found page

--- react-coin/src/index.js
+++ react-coin/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/List';
+import NotFound from './components/notfound/NotFound';
 import './index.css';
 
 const App = () => {
     return (
-        <div>
-            <Header/>
+        <BrowserRouter>
+            <div>
+                <Header/>
 
-            <List/>
-        </div>
+                <Switch>
+                    <Route path="/" component={List} exact/>
+                    
+                    <Route component={NotFound}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
