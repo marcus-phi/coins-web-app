8cb11a0f43df3bd58fbda4086e08dd5ec4ba7781
phixuannhat <phixuannhat@gmail.com>
2018-04-16 14:43:50 -0500

    pagination with start/end page

--- vue-coin/src/components/list/List.vue
+++ vue-coin/src/components/list/List.vue
@@ -2,7 +2,16 @@
     <div>
         <div v-if="loading" class="loading-container"><Loading/></div>
         <div v-else-if="error" class="error">{{error}}</div>
-        <Table v-else :currencies="currencies"/>
+
+        <div v-else>
+            <Table :currencies="currencies"/>
+
+            <Pagination
+                :page="page"
+                :totalPages="totalPages"
+                :handlePaginationClick="handlePaginationClick"
+            />
+        </div>
     </div>
     
 </template>
@@ -10,6 +19,7 @@
 <script>
 import Loading from '../common/Loading.vue';
 import Table from './Table.vue';
+import Pagination from './Pagination.vue';
 import { API_URL } from '../../config';
 import { handleResponse } from '../../helpers';
 
@@ -17,28 +27,47 @@ export default {
     components: {
         Loading,
         Table,
+        Pagination,
     },
     data: function() {
         return {
             loading: false,
             currencies: [],
             error: null,
+            totalPages: 0,
+            page: 1,
+        }
+    },
+
+    methods: {
+        fetchCurrencies: function() {
+            this.loading = true;
+
+            fetch(`${API_URL}/cryptocurrencies?page=${this.page}&perPage=20`)
+            .then(handleResponse)
+            .then(data => {
+                this.loading = false;
+                this.currencies = data.currencies;
+                this.totalPages = data.totalPages;
+            })
+            .catch(error => {
+                this.loading = false;
+                this.error = error.errorMessage;
+            });
+        },
+        handlePaginationClick: function(page) {
+            this.page = page;
+        }
+    },
+
+    watch: {
+        page: function() {
+            this.fetchCurrencies();
         }
     },
 
     mounted: function() {
-        this.loading = true;
-
-        fetch(`${API_URL}/cryptocurrencies?page=1&perPage=20`)
-        .then(handleResponse)
-        .then(data => {
-            this.loading = false;
-            this.currencies = data.currencies;
-        })
-        .catch(error => {
-            this.loading = false;
-            this.error = error.errorMessage;
-        });
+        this.fetchCurrencies();
     }
 }
 </script>
---
+++ vue-coin/src/components/list/Pagination.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="Pagination">
+        <button
+            class="Pagination-button"
+            @click="handlePaginationClick(1)"
+            :disabled="page <= 1">
+            ⇤
+        </button>
+
+        <button
+            class="Pagination-button"
+            @click="handlePaginationClick(page-1)"
+            :disabled="page <= 1">
+            &larr;
+        </button>
+
+        <span class="Pagination-info">
+            page <b>{{page}}</b> of <b>{{totalPages}}</b>
+        </span>
+
+        <button
+            class="Pagination-button"
+            @click="handlePaginationClick(page+1)"
+            :disabled="page >= totalPages">
+            &rarr;
+        </button>
+
+        <button
+            class="Pagination-button"
+            @click="handlePaginationClick(totalPages)"
+            :disabled="page >= totalPages">
+            ⇥
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        page: {
+            type: Number,
+            required: true,
+        },
+        totalPages: {
+            type: Number,
+            required: true,
+        },
+        handlePaginationClick: {
+            type: Function,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style>
+.Pagination {
+  margin: 50px auto;
+  text-align: center;
+}
+
+.Pagination-button {
+  text-align: center;
+  border: none;
+  border-radius: 16px;
+  background-color: #4997e5;
+  transition: background-color .2s;
+  color: white;
+  cursor: pointer;
+  margin: 10px;
+  width: 44px;
+  height: 34px;
+}
+
+.Pagination-button:hover {
+  background-color: #457cb2;
+}
+
+.Pagination-button:focus {
+  outline: none;
+}
+
+.Pagination-button:disabled {
+  background-color: #1f364d;
+  cursor: not-allowed;
+}
+
+.Pagination-info {
+  font-size: 12px;
+}
+</style>
